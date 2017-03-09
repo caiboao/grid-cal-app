0cfbea27f52385a2b94fe44cf63b3813ef887db7
kylehu16 <deanhh9@gmail.com>
2017-03-09 18:03:15 +0800

    增加玩家对象

---
+++ src/app/grid-detail.component.ts
@@ -0,0 +1,12 @@
+import {Component,Input} from '@angular/core';
+
+import{Player} from './player';
+
+@Component({
+    selector:'grid-detail',
+    templateUrl:'./grid-detail.component.html'
+})
+
+export class GridDetailComponent{
+
+}
