13e14fac4a2b2847aba9c95c74a723d057efeabc
kylehu16 <deanhh9@gmail.com>
2017-03-20 17:53:46 +0800

    修改

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -5,7 +5,10 @@ import {Player} from './player';
 @Injectable()
 export class AppService{
     allPlayers:Player[]=[]; //所有玩家
-    addPlayer():void{
-        var i=1;
+    addPlayer(newplayer:Player):void{
+        this.allPlayers.push(newplayer);
+    }
+    restart(){
+
     }
 }
